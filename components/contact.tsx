b2ccf842b466a19e06fd9c4dff32388186ce6a27
Abdellatif Rhahli <89397304+abdorhl@users.noreply.github.com>
2024-05-03 16:15:40 +0100

    Update contact

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,8 +33,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:abdellatifrhahli28@gmail.com">
-          abdellatifrhahli28@gmail.com
+        <a className="underline" href="mailto:contacts@abdellatifrhahli.tech">
+             contacts@abdellatifrhahli.tech
         </a>{" "}
         or through this form.
       </p>
