0e7f7e8263fbd72d72f29cb884937b408ced7e68
abdorhl <abdltif.rhl28@gmail.com>
2025-05-07 12:10:09 +0100

    WIPO

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,6 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-  },
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
+  testEnvironment: 'node',
+  transform: {},
+  testMatch: ['**/__tests__/**/*.test.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/']
 }; 
